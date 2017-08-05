c1d430f773894edb259b8120f55a6a825c38d53b
Ryan Helsing <ryanhelsing@gmail.com>
2017-08-05 12:42:24 -0400

    fresh

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h2>Welcome to React</h2>
+        <h2>React Template</h2>
         <p>To get started, edit <code>src/components/app.js</code> and save to reload.</p>
       </div>
     );
